6d999a22d92b2cba6212c3d8614d7560d3c23a40
YubeRodri <yrodriguez@uamv.edu.ni>
2025-11-04 18:25:53 -0600

    Partes 3, 4 y 5 de los videos POO

---
+++ src/main/java/com/tuempresa/facturacion/calculadores/CalculadorSiguienteNumeroParaAnyo.java
@@ -0,0 +1,4 @@
+package com.tuempresa.facturacion.calculadores;
+
+public class CalculadorSiguienteNumeroParaAnyo {
+}
---
+++ src/main/java/com/tuempresa/facturacion/modelo/Factura.java
@@ -0,0 +1,4 @@
+package com.tuempresa.facturacion.modelo;
+
+public class Factura {
+}
